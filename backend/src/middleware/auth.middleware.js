b02d888fb6db3654fdbba783959684e00061436e
divyanshbisht <145752427+divyanshbisht@users.noreply.github.com>
2025-12-09 23:35:39 +0530

    message routes optimized (and spellings changed 'middleware')

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -18,7 +18,7 @@ export const protectRoute = async (req, res, next) => {
         next()
 
     } catch (error) {
-        console.log("Error in protectRoute middlware: ", error)
+        console.log("Error in protectRoute middleware: ", error)
         res.status(500).json({message: "Internal server error"});
 
     }
