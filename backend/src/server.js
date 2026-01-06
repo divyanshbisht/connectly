8d108accaa97a38ba2bc49732c3a2fbc00b2b44d
divyanshbisht <145752427+divyanshbisht@users.noreply.github.com>
2026-01-06 20:16:27 +0530

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -17,21 +17,22 @@ const PORT = ENV.PORT || 3000;
 
 //payload too large error (fix by adding limit parameter)
 app.use(express.json()) // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
 
 // make ready for deployment
-if(ENV.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (ENV.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (_,res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
 }
 
-app.listen(PORT, "0.0.0.0", () =>{
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
   connectDB()
 }
