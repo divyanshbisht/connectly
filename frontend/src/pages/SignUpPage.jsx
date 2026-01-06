974e6f99c6d5a830c641c5f4dd43a9c959b26987
divyanshbisht <145752427+divyanshbisht@users.noreply.github.com>
2026-01-06 20:51:12 +0530

    login&logout implemented

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -75,7 +75,7 @@ function SignUpPage() {
                   <div>
                     <label className='auth-input-label'>Password</label>
                     <div className='relative'>
-                      <MailIcon className='auth-input-icon' />
+                      <LockIcon className='auth-input-icon' />
                       <input
                         type="password"
                         value={formData.password}
@@ -110,7 +110,7 @@ function SignUpPage() {
             </div>
 
             {/* FORM ILLUSTRATION - RIGHT SIDE */}
-            <div className='hideen md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent'>
+            <div className='hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent'>
               <div>
                 <img
                   src="/signup.png"
