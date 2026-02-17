78b2f4007b78a58e8e262eece22856416abe3e3f
divyanshbisht <145752427+divyanshbisht@users.noreply.github.com>
2026-02-18 02:36:25 +0530

    frontend ready for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",     (made a frontend env)
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
-});
+}); 
